5f1a1eeb5e354b7f8e408f7e2214529e89f9f5cc
Zachary Fetters <zachary.fetters@yahoo.com>
2020-04-04 16:18:50 -0400

    new key

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 function setEmail() { //function to (hopefully) stop email scraper bots. Sets href on body load.
-  document.getElementById("email").href = "mailto:zachary.fetters@yahoo.com?subject=Design%20Project%20Request%20%E2%80%94%20YOUR%20NAME%20HERE&body=Details%20about%20your%20project%20request%20go%20here.%20Things%20like%20design%20ideas%2C%20branding%20details%2C%20and%20soft%20deadlines%20are%20highly%20recommended.%20The%20more%20details%20provided%2C%20the%20more%20interested%20I%20become%20in%20the%20project!";
+  document.getElementById("email").href = "mailto:zachfett@protonmail.com?subject=Design%20Project%20Request%20%E2%80%94%20YOUR%20NAME%20HERE&body=Details%20about%20your%20project%20request%20go%20here.%20Things%20like%20design%20ideas%2C%20branding%20details%2C%20and%20soft%20deadlines%20are%20highly%20recommended.%20The%20more%20details%20provided%2C%20the%20more%20interested%20I%20become%20in%20the%20project!";
   return true;
 }
 
