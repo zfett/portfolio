dc9c8dab54e9e8e60e72650f387cd74082d496da
Zachary Edward Harrison Fetters <zachfett@protonmail.com>
2020-05-30 21:21:51 -0400

    updates

--- sct/src/elem.js
+++ sct/src/elem.js
@@ -6,7 +6,6 @@ class FeaturedCover extends HTMLElement {
     wrapper.setAttribute('class','wrapper');
     var image = document.createElement('span');
     image.setAttribute('class', 'image');
-    image.setAttribute('tabindex', 0);
     var imgUrl;
     if(this.hasAttribute('src')) {
       imgUrl = this.getAttribute('src');
@@ -19,4 +18,24 @@ class FeaturedCover extends HTMLElement {
   }
 }
 
-customElements.define('featured-cover', FeaturedCover);
+class ResultImage extends HTMLElement {
+  constructor() {
+    super();
+    var shadow = this.attachShadow({mode: 'open'});
+    var wrapper = document.createElement('span');
+    wrapper.setAttribute('class','wrapper');
+    var image = document.createElement('span');
+    image.setAttribute('class', 'image');
+    var imgUrl;
+    if(this.hasAttribute('src')) {
+      imgUrl = this.getAttribute('src');
+    }
+    var img = document.createElement('img');
+    img.src = imgUrl;
+    image.appendChild(img);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(image);
+  }
+}
+
+customElements.define('result-image', ResultImage);
