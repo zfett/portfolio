b7a5b7f933c616a27e4e3ee2e92979999fbcbdb2
Zachary Fetters <zachfett@protonmail.com>
2020-06-30 18:14:44 -0400

    updates

--- webtools/static/script/global.js
+++ webtools/static/script/global.js
@@ -1,8 +1,33 @@
-function toggleMenu() {
+function toggleMenu() { //Toggle menu element
   const menuElem = document.getElementById("header-menu");
   if (menuElem.classList.contains("active")) {
     menuElem.classList.remove("active");
   } else if (!menuElem.classList.contains("active")) {
     menuElem.classList.add("active");
   }
+}
+
+document.addEventListener("click", function(event) { //Check if menu is clicked out of
+  const buttonElem = document.getElementsByClassName("header-menu-toggle")[0];
+  const menuElem = document.getElementById("header-menu");
+
+  var isClickInsideMenu = menuElem.contains(event.target);
+  var isClickInsideButton = buttonElem.contains(event.target);
+
+  if (!isClickInsideMenu && !isClickInsideButton) {
+    menuElem.classList.remove("active");
+  }
+});
+
+window.onload = function() {
+  const hrefElem = document.body.querySelectorAll("[href]");
+  for (i = 0; i < hrefElem.length; i++) {
+    var link = hrefElem[i].getAttribute("href");
+    hrefElem[i].setAttribute("onclick", "window.open('"+link+"', '_blank')");
+    hrefElem[i].removeAttribute("href");
+    hrefElem[i].title = "Navigate to '"+link+"' in a new tab.";
+  }
+
+  $("header-content").load("../../static/template/header.html");
+  $("footer-content").load("../../static/template/footer.html");
 }
---
+++ webtools/tool/cardvalidator/tool.js
@@ -0,0 +1,5 @@
+document.onload = function() {
+  $("header-content").load("../../static/template/header.html");
+  $("footer-content").load("../../static/template/footer.html");
+  console.log("load");
+}
