f373e9924a365f3ee98eee3fa99de94194360f18
Zachary Fetters <zachfett@protonmail.com>
2020-10-07 14:42:32 -0400

    new design

--- res/src/index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-var header = document.getElementById("page_header");
-function myFunction() {
-  if (window.pageYOffset > window.innerHeight) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-} 
