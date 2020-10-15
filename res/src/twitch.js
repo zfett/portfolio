328122f2928b3ddafab554149ac19222da8622b2
Zachary Fetters <zachfett@protonmail.com>
2020-10-15 15:45:45 -0400

    fix

--- res/src/twitch.js
+++ res/src/twitch.js
@@ -1,7 +1,7 @@
 const liveElem = document.getElementsByClassName("live-tag")[0];
 $.ajax({
 	type: "POST",
-	url: "https://id.twitch.tv/oauth2/token?client_id="+process.env.TWITCH_CLIENT_ID+"&client_secret="+process.env.TWITCH_AUTH_KEY+"&grant_type=client_credentials",
+	url: "https://id.twitch.tv/oauth2/token?client_id="+{process.env.TWITCH_CLIENT_ID}+"&client_secret="+{process.env.TWITCH_AUTH_KEY}+"&grant_type=client_credentials",
 	async: false,
 	dataType: 'json',
 	success: function (data){
@@ -12,7 +12,7 @@ $.ajax({
 			async: false,
 			headers: {
 				"Authorization": "Bearer "+data.access_token,
-				"client-id": process.env.TWITCH_CLIENT_ID
+				"client-id": {process.env.TWITCH_CLIENT_ID}
 			},
 			success: function (result){
 				if (result.data[0].is_live === false) {
