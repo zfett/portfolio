0affa894e9487dc769267d4fcceb15a344acda6e
Zachary Edward Harrison Fetters <zachfett@protonmail.com>
2020-10-15 18:15:05 -0400

    final

--- res/src/twitch.js
+++ res/src/twitch.js
@@ -1,4 +1,5 @@
 const liveElem = document.getElementsByClassName("live-tag")[0];
+const liveCont = document.getElementById("live");
 $.ajax({
 	type: "POST",
 	url: "https://id.twitch.tv/oauth2/token?client_id="+process.env.TWITCH_CLIENT_ID+"&client_secret="+process.env.TWITCH_AUTH_KEY+"&grant_type=client_credentials",
@@ -15,10 +16,11 @@ $.ajax({
 				"client-id": process.env.TWITCH_CLIENT_ID
 			},
 			success: function (result){
+				liveElem.title = "I'm live on Twitch: "+result.data[0].title;
 				if (result.data[0].is_live === false) {
-					return false;
+					liveCont.remove();
 				} else if (result.data[0].is_live === true) {
-					return true;
+					liveElem.classList.add("active");
 				}
 			}
 		});
