574d93687f186d9e5f93b5d7ff417897d134eb49
zfett <zachfett@protonmail.com>
2020-11-03 23:21:56 -0500

    slow progress
    to-do:
    - need portfolio images
    - contact info/form
    - finish skills section
    - add more about info

--- res/script/index.js
+++ res/script/index.js
@@ -1,4 +1,11 @@
+console.info("%cRecruiters & Employers: I'm looking for work if you need a front-end designer! @zfett on LinkedIn", "color: limegreen; font-size: 20px;");
+
 const _SECTION = document.getElementsByTagName("section");
 for (i=1;i<_SECTION.length;i++) {
-	_SECTION[i].style.animationDelay = "1."+i+"s";
+	_SECTION[i].style.animationDelay = "1."+(i-1)+"s";
+}
+
+const _SOCIAL = document.getElementsByClassName("social");
+for (i=0;i<_SOCIAL.length;i++) {
+	_SOCIAL[i].style.animationDelay = (1+i*.2)+"s";
 }
