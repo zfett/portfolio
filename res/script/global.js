b9feadf18bca3e918d15e7b346ead3b1e9fe7e78
Zachary Edward Harrison Fetters <zachfett@protonmail.com>
2020-12-30 16:07:30 -0500

    ch, ch, ch, ch, changes

--- res/script/global.js
+++ res/script/global.js
@@ -8,3 +8,28 @@ if (y == "2020") {
 } else {
   curDateElem.innerHTML = " - "+y;
 }
+
+let isPlaying = false;
+const audioSrc = document.getElementById("audio_src");
+const audioToggle = document.getElementById("audio_toggle");
+
+function toggleAudio() {
+  if (isPlaying) {
+    isPlaying = false;
+    audioSrc.pause();
+    audioSrc.volume = 0;
+    audioToggle.style.backgroundImage = "url('res/image/icon/speaker_off.png')";
+  } else if (!isPlaying) {
+    isPlaying = true;
+    audioSrc.play();
+    audioSrc.volume = 0.4;
+    audioToggle.style.backgroundImage = "url('res/image/icon/speaker_on.png')";
+  }
+  
+  audioSrc.addEventListener('ended', (event) => {
+    isPlaying = false;
+    audioToggle.style.backgroundImage = "url('res/image/icon/speaker_off.png')";
+  });
+}
+
+audioToggle.addEventListener("click", toggleAudio);
