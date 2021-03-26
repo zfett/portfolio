a134a1c60ca6d812f24621378791fa40bec4c795
Zachary Fetters <zachfett@protonmail.com>
2021-03-26 11:44:27 -0400

    removed textstyl

--- projects/textstyl/global.js
+++
@@ -1,7 +0,0 @@
-const header_toggle = document.getElementById("header_toggle");
-const header_links = document.getElementById("header_links");
-
-header_toggle.addEventListener("click", function() {
-  header_toggle.classList.toggle("active");
-  header_links.classList.toggle("active");
-})
