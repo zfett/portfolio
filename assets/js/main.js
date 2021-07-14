e53e9cb679885c742b74b887ca14051a7dc55ec9
Zachary Fetters <zachfett@protonmail.com>
2021-07-14 15:10:45 -0400

    misc. design tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,15 @@ window.addEventListener("DOMContentLoaded", function() {
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   ageElem.innerHTML = Math.floor(diffDays / 365);
 })
+
+const tx = document.getElementsByTagName("textarea");
+
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
